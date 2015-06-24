8ee1c774efa079464913bb082bca32f5613126c1
Edoardo Rossi <edd.rossi@gmail.com>
2015-06-24 15:22:11 +0200

    Define a wrapper to rescue/retry queries and connections.

--- lib/polipus-cassandra/queue_overflow/cassandra_queue.rb
+++ lib/polipus-cassandra/queue_overflow/cassandra_queue.rb
@@ -200,6 +200,48 @@ module Polipus
         props = properties.to_a.join(' AND ')
         statement = props.empty? ? "#{def_};" : "#{def_} WITH #{props};"
         session.execute statement
+      private
+
+      RESCUED_CASSANDRA_EXCEPTIONS = [
+        ::Cassandra::Errors::ExecutionError,
+        ::Cassandra::Errors::IOError,
+        ::Cassandra::Errors::InternalError,
+        ::Cassandra::Errors::NoHostsAvailable,
+        ::Cassandra::Errors::ServerError,
+        ::Cassandra::Errors::TimeoutError
+      ]
+      # Trying to rescue from a Cassandra::Error
+      #
+      # The relevant documentation is here (version 2.1.3):
+      # https://datastax.github.io/ruby-driver/api/error/
+      #
+      # Saving from:
+      #
+      # - ::Cassandra::Errors::ExecutionError
+      # - ::Cassandra::Errors::IOError
+      # - ::Cassandra::Errors::InternalError
+      # - ::Cassandra::Errors::NoHostsAvailable
+      # - ::Cassandra::Errors::ServerError
+      # - ::Cassandra::Errors::TimeoutError
+      #
+      # Ignoring:
+      # - Errors::ClientError
+      # - Errors::DecodingError
+      # - Errors::EncodingError
+      # - Errors::ValidationError
+      #
+      # A possible and maybe-good refactoring could be refine for the
+      # network related issues.
+      def attempts_wrapper(attempts = 2, &block)
+        (1..attempts).each do |i|
+          begin
+            return block.call() if block_given?
+          rescue *RESCUED_CASSANDRA_EXCEPTIONS => e
+            logger.error { "(#{i}/#{attempts} attempts) Bad fail! Retry in #{i*2} seconds to recover  #{e.class.name}: #{e.message}" }
+            sleep(i*2)
+          end
+        end
+        nil
       end
     end
   end
