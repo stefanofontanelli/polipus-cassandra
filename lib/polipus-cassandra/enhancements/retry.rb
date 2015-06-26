20f2b9b0fb0c4acca6dbd65d558eec586eb0184c
Edoardo Rossi <edd.rossi@gmail.com>
2015-06-26 17:16:50 +0200

    Refactoring: improve readability.

--- lib/polipus-cassandra/enhancements/retry.rb
+++ lib/polipus-cassandra/enhancements/retry.rb
@@ -8,6 +8,9 @@ module Polipus
         Logger.new(STDOUT).tap { |l| l.level = Logger::INFO } unless logger
       end
 
+      module_function :enhancements_logger
+      public :enhancements_logger
+
       RESCUED_CASSANDRA_EXCEPTIONS = [
         ::Cassandra::Errors::ExecutionError,
         ::Cassandra::Errors::IOError,
@@ -45,14 +48,14 @@ module Polipus
             return block.call() if block_given?
           rescue *RESCUED_CASSANDRA_EXCEPTIONS => e
             sleep_for = i * Random.rand(1.5..2.5)
-            self.enhancements_logger.error { "(#{i}/#{attempts} attempts) Bad fail! Retry in #{sleep_for} seconds to recover  #{e.class.name}: #{e.message}" }
+            enhancements_logger.error { "(#{i}/#{attempts} attempts) Bad fail! Retry in #{sleep_for} seconds to recover  #{e.class.name}: #{e.message}" }
             sleep(sleep_for)
           end
         end
         nil
       end
 
-      module_function(:attempts_wrapper)
+      module_function :attempts_wrapper
       public :attempts_wrapper
     end
   end
